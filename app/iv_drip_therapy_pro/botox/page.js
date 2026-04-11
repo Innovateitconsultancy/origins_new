d187c3eeb54601000ba0d84fbcb01ab464b21b4d
Mukesh <mukeshnyaykar@gmail.com>
2026-04-11 15:57:28 +0530

    Membership

--- app/iv_drip_therapy_pro/botox/page.js
+++ app/iv_drip_therapy_pro/botox/page.js
@@ -122,7 +122,7 @@ export default function ImmunityRecovery() {
                                                    <div className="row g-4 justify-content-between topBottm30">
                                                            <div className="col-lg-6">
                                                                <div className="details-image">
-                                                                   <img className="radiusImg" src="/assets/img/therapy/blood_boost_iron_therapy_img.jpg" alt="therapy-img" />
+                                                                   <img className="radiusImg" src="/assets/img/therapy/botox_img.png" alt="therapy-img" />
                                                                                                    </div>
                                                            </div>
                                                        
